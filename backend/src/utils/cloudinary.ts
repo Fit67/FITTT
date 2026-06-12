4690a3d10ddff1c4d4365194717840d12bbc1e87
Fit67 <janazaapp1@gmail.com>
2026-06-12 22:27:32 +0300

    Allow separate Cloudinary environment variables

--- backend/src/utils/cloudinary.ts
+++ backend/src/utils/cloudinary.ts
@@ -1,6 +1,14 @@
 import { v2 as cloudinary, UploadApiResponse } from 'cloudinary'
 import streamifier from 'streamifier'
 
+// Explicitly configure so it can read individual env vars or CLOUDINARY_URL
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
+});
+
 export async function uploadImageBuffer(buffer: Buffer, folder: string = 'products'): Promise<UploadApiResponse> {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
